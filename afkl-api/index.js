4d26f54c2328eca67e32344e2b84564689a94e8e
csaba szabo <csabattilaszabo@gmail.com>
2021-10-31 13:01:20 +0100

    validate server request

--- afkl-api/index.js
+++ afkl-api/index.js
@@ -3,15 +3,25 @@ const app = express()
 const port = process.env.PORT || 3001
 
 app.get('/booking', (req, res) => {
-    const data = require('./mock/mock.json').find((booking) => {
-        return booking.bookingCode.toLowerCase() === req.query.bookingCode.toLowerCase() &&
-            booking.passengers.some(passenger => passenger.lastName.toLowerCase() === req.query.lastName.toLowerCase())
-    });
-
-    if (!data) {
-        res.status(404).send('Not found');
+    console.log(req.query.bookingCode);
+    if (
+        !req.query.lastName ||
+        !req.query.bookingCode ||
+        req.query.bookingCode.length < 5 ||
+        req.query.bookingCode.length > 6
+    ) {
+        res.status(400).send('Bad request');
     } else {
-        res.json(data);
+        const data = require('./mock/mock.json').find((booking) => {
+            return booking.bookingCode.toLowerCase() === req.query.bookingCode.toLowerCase() &&
+                booking.passengers.some(passenger => passenger.lastName.toLowerCase() === req.query.lastName.toLowerCase())
+        });
+
+        if (!data) {
+            res.status(404).send('Not found');
+        } else {
+            res.json(data);
+        }
     }
 })
 
